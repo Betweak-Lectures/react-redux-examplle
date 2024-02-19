2c60189b51e5e0b38a7a767e53b53ef46b57c0b9
BeTweak <ys@betweak.com>
2024-02-19 13:33:28 +0900

    initial commit

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore } from "redux";
+import counterReducer from "./reducers/counter";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const store = createStore(rootReducer);
+// console.log(store.getState());
+// console.log(store.dispatch);
+
+export default store;
